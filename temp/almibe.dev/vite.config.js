6c1473ab1e3699ff1992e81cdfd69b2dbb7450b2
Alex Michael Berry <alexmiberry@gmail.com>
2023-12-14 05:01:10 -0500

    adding new generator

---
+++ temp/almibe.dev/vite.config.js
@@ -0,0 +1,73 @@
+import { defineConfig } from 'vite';
+import { resolve, basename } from 'path';
+import fs from 'fs';
+import walk from 'walkdir';
+import { marked } from 'marked';
+import Mustache from 'mustache';
+
+export default defineConfig({
+    plugins: [
+      staticSiteGen(),
+    ],
+    build: {
+      rollupOptions: {
+        input: [],
+      },
+    },
+});
+
+function staticSiteGen() {
+  return {
+    name: 'static-site-gen',
+
+    moduleParsed(id) {
+      if (id.id.endsWith(".html")) {
+          console.log(JSON.stringify(id.id))
+          id.id = id.id.replace("pages", "")
+      }
+      return id
+    },
+
+    buildStart(inputOptions) {
+      let htmlFiles = []
+      let paths = walk.sync("./pages");
+
+      paths.forEach(function (path) {
+        if (path.endsWith(".md")) {
+          let content = fs.readFileSync(path, 'utf-8')
+          let data = {}
+          let lines = content.split(/\r?\n/);
+          if (lines[0] === "---") { // This file contains metadata
+            content = ""
+            let endHeader = false
+            for (const [i, line] of lines.entries()) {
+              if (endHeader) {
+                content += line + "\n"
+              } else {
+                if (line === "---") {
+                  endHeader = true && i != 0;
+                } else {
+                  let [key, ...value] = line.split(':')
+                  value = value.join(':')
+                  data[key] = value
+                }
+              }
+            }
+          }
+          data["body"] = marked.parse(content);
+          let template = fs.readFileSync(resolve(__dirname, "layout/Main.html"), 'utf-8');
+          let output = Mustache.render(template, data)
+          let fileName = path.replace(".md", ".html");
+          htmlFiles.push(fileName)
+          fs.writeFileSync(fileName, output)
+        }
+      });
+      inputOptions.input = htmlFiles
+    },
+
+    // Handles live code changes to content files.
+    handleHotUpdate(ctx) {
+      console.log(`In HHU ${ctx}`)
+    },
+  }
+}
